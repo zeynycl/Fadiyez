fdc33a881e502e403282ebf0c1acfb30dafba7f0
zeynycl <zeynycl@gmail.com>
2025-12-30 17:46:32 +0300

    Update kullanicilar.sql

--- kullanicilar.sql
+++ kullanicilar.sql
@@ -1,1 +1,6 @@
-SELECT * FROM projedeneme.kullanicilar;
+'CREATE TABLE `kullanicilar` (
+  `id` int NOT NULL,
+  `kullanici_adi` varchar(50) DEFAULT NULL,
+  `sifre` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) '
