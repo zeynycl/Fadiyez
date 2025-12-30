d106286f384bdb3b9a012e8e2046b4e130e16aec
zeynycl <zeynycl@gmail.com>
2025-12-30 17:47:56 +0300

    Update urunler.sql

--- urunler.sql
+++ urunler.sql
@@ -1,1 +1,9 @@
-SELECT * FROM projedeneme.urunler;
+'CREATE TABLE `urunler` (
+  `idurunler` int NOT NULL,
+  `ad` varchar(45) DEFAULT NULL,
+  `kategori` varchar(45) DEFAULT NULL,
+  `fiyat` decimal(10,2) DEFAULT NULL,
+  `stok` int DEFAULT ''50'',
+  `hedef_stok` int DEFAULT ''50'',
+  PRIMARY KEY (`idurunler`)
+) '
